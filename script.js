348488c42db28d7560633111aaa98b232a72eada
Gabriel Guimarães <gabrielguimaraes.dev@gmail.com>
2022-05-10 05:11:02 -0300

    FEITO ATE EXERCICIO 19

---
+++ script.js
@@ -0,0 +1,23 @@
+const checkAgreement = document.getElementById('agreement');
+const btnSubmit = document.getElementById('submit-btn');
+
+function agreementCheck() {
+  if (checkAgreement.checked) {
+    btnSubmit.disabled = false;
+  } else {
+    btnSubmit.disabled = true;
+  }
+};
+agreementCheck();
+checkAgreement.addEventListener('click', agreementCheck)
+
+const botaoDeLogin = document.querySelector('#login')
+botaoDeLogin.addEventListener('click', function() {
+  const email = document.querySelector('#email');
+  const senha = document.querySelector('#senha');
+  if(email.value === 'tryber@teste.com' && senha.value === '123456') {
+    window.alert('Olá, Tryber!');
+  } else {
+    window.alert('Email ou senha inválidos.');
+  }
+});
