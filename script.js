d605a75f0026c536766e6e21a31682d6d04bdc03
Gabriel Guimarães <gabrielguimaraes.dev@gmail.com>
2022-05-10 00:35:30 -0300

    ATT

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-function selectHouse (){
-    let selectOptions = [
-        {'text':'Gitnória', 'value':'Gitnória', 'id':'gitnoria-house', 'innerHTML': 'Gitnória'},
-        {'text':'Reactpuff', 'value':'Reactpuff', 'id':'reactpuff-house', 'innerHTML': 'Reactpuff'},
-        {'text':'Corvinode', 'value':'Corvinode', 'id':'corvinode-house', 'innerHTML': 'Corvinode'},
-        {'text':'Pytherina', 'value':'Pytherina', 'id':'pytherina-house', 'innerHTML': 'Pytherina'},
-    ]
-}
+.
